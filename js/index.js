8a9ef2783c0f6c2a1228e7dd0efe4b11ee1be415
Eldar Heleg <eldar.heleg@gmail.com>
2023-05-01 21:56:40 +0200

    passenger counter done

--- js/index.js
+++ js/index.js
@@ -1,16 +1,38 @@
 // document.getElementById("count").innerText = 5
 
-let first = 5 
+// let first = 5
 
-let count = first
+// let count = first
 
-console.log(count)
+// console.log(count)
 
 //1
 //console.log(myAge) cannot access
 
-let myAge = 23
+// let myAge = 23
 
-myAge++
+// myAge++
 
-console.log(myAge)
+// console.log(myAge)
+
+let countEl = document.getElementById("count-id")
+
+//console.log(countEl)
+
+let count = 0
+
+function increment() {
+    count = count + 1 
+    countEl.innerText = count
+}
+
+let countS = 0
+let string = document.getElementById("string")
+
+//console.log(string)
+
+function save() {
+    countS = countEl.innerText
+    string.innerText = string.innerText + " " + countS + " - "
+}
+
