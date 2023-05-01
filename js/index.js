e0cac238076014fd9ee642100c7cfb24c97ffc03
Eldar Heleg <eldar.heleg@gmail.com>
2023-05-01 11:03:33 +0200

    scrimba course

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// document.getElementById("count").innerText = 5
+
+let first = 5 
+
+let count = first
+
+console.log(count)
+
+//1
+//console.log(myAge) cannot access
+
+let myAge = 23
+
+myAge++
+
+console.log(myAge)
